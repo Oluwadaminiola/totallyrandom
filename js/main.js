a2b6e6900318667a8aa1bd72b5336a66d2f72c68
unknown <pelumioni25@gmail.com>
2019-02-14 18:04:20 +0100

    totallyrandom

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    // $(window).scroll(function() {
+    // if ($(document).scrollTop() > 100) {
+    //     $('nav').addClass('chade');
+    //   } else {
+    //     $('nav').removeClass('chade');
+    //   }
+    // });
+    AOS.init({
+        duration: 1200,
+      })
+    }
